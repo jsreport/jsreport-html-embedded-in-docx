8cc768166e297bc2408ed067496293366c1d9dcb
pofider <honza.pofider@seznam.cz>
2019-07-12 10:47:55 +0200

    update deps

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,8 +1,8 @@
 const office = require('jsreport-office')
 
 module.exports = {
-  'name': 'html-embedded-in-docx',
-  'main': 'lib/main.js',
-  'dependencies': ['templates'],
-  'optionsSchema': office.extendSchema('html-embedded-in-docx', {})
+  name: 'html-embedded-in-docx',
+  main: 'lib/main.js',
+  dependencies: ['templates'],
+  optionsSchema: office.extendSchema('html-embedded-in-docx', {})
 }
